e512c2ec097ccb010a73f19a396e1de0af16ba82
Ethan Roberts <theLameBrain@gmail.com>
2021-04-02 10:42:45 -0700

    oooooh yeah. feel the slither

---
+++ Assets/Scripts/Controllers/IntroScreenController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroScreenController : MonoBehaviour
+{
+    public AudioSource hisssss;
+
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = Random.Range(100f, 1500f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer--;
+        if(timer < 0)
+        {
+            timer = Random.Range(1000f, 15000f);
+            hisssss.PlayOneShot(hisssss.clip, Random.Range(0f, .5f));
+        }
+    }
+}
