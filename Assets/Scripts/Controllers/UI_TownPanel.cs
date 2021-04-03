aac9bebf33a4ec791c218834d1b99a6c83a73e0e
Ethan Roberts <theLameBrain@gmail.com>
2021-04-03 01:22:38 -0700

    Need to choose a target in temple. need Inn screen Need to hit space and cancel out of signs so many things left

---
+++ Assets/Scripts/Controllers/UI_TownPanel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_TownPanel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CloseTown()
+    {
+        GameManager.PARTY.SetAllowedMovement(true);
+        Destroy(this.gameObject);
+    }
+}
